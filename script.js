5056776cd9b0608373491faec45fac8fa657fe32
Prabhjot Sidhu <sidhu.prabhjot.2003@gmail.com>
2022-07-22 15:14:08 -0400

    inputs added

--- script.js
+++ script.js
@@ -6,8 +6,28 @@ let projectileCalcBtn = document.querySelector(".projectile-calc-btn");
 let speedCalc = document.querySelector(".calc-one");
 let projectileCalc = document.querySelector(".calc-two");
 
-//click event handler for calculator buttons
+//input fields for the uniform acceleration calculator
+let initVelocityUA = document.querySelector("#velocity-i-ua");
+
+//calculate button
+let calculateBtn = document.querySelector(".calculate-btn");
+
+//final answer display
+let answer = document.querySelector(".answer");
+
+//click event handler for calculator buttons that toggle between the types
 speedCalcBtn.addEventListener("click", () => {
   speedCalc.style.display = "block";
   projectileCalc.style.display = "none";
+});
+
+projectileCalcBtn.addEventListener("click", () => {
+  speedCalc.style.display = "none";
+  projectileCalc.style.display = "block";
+});
+
+//final calculate button function
+calculateBtn.addEventListener("click", () => {
+  let initVelocityUAText = initVelocityUA.value;
+  answer.innerText = initVelocityUAText;
 });
